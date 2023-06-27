58f47c4ee46a6f3ecd051de01e232ecef374805a
Ovod2013 <Ovod2013@yandex.ru>
2023-06-27 22:43:19 +0300

    Второй рабочий вариант.

--- task01/Program.cs
+++ task01/Program.cs
@@ -15,120 +15,102 @@
 
 Console.Clear();
 Console.WriteLine();
-string[] res = new string[4];
+
+int dlMassiva;
+
+Console.WriteLine($"Если вы желаете самостоятельно ввести элементы массива,");
+Console.Write($"введите 'Д' или 'Y', иначе введите любой другой символ: ");
+string otvet=Console.ReadLine()!;
+
+if (otvet=="Д" | otvet=="д" | otvet== "Y" | otvet == "y")
+{
+    Console.WriteLine($"Введите длину массива в виде числа от 1 до 65000: ");
+    dlMassiva = int.Parse(Console.ReadLine()!);
+    otvet="Д";
+}
+else dlMassiva=4;
+
+string[] res = new string[dlMassiva];
+// res[0]="Hello";
+// res[1]="2";
+// res[2]="world";
+// res[3]=":-)";
+
+if (otvet=="Д")
+{
+    for (int i=0; i<dlMassiva; i++)
+    {
+        Console.Write($"Введите {i} элемент массива: ");
+        res[i]=Console.ReadLine()!;
+    }
+}
+else 
+{
+//res[]=["Hello","2", "3", "в"];
 res[0]="Hello";
 res[1]="2";
 res[2]="world";
 res[3]=":-)";
-
-Console.WriteLine($"Вы желаете самостоятельно ввести элементы массива от 0 до 3?");
-Console.Write($"Введите 'Да' или 'Нет' : ");
-string otvet=Console.ReadLine()!;
-
-if (otvet=="Да")
-{
-    for (int i=0; i<4; i++)
-{
-    Console.Write($"Введите {i} элемент массива: ");
-    res[i]=Console.ReadLine()!;
 }
-}
-else 
 
-for (int j=0; j<4; j++)
+for (int j=0; j<dlMassiva; j++)
 {
     Console.WriteLine($"{j} элемент массива: {res[j]}");
 }
 
 
-
+//int dl=dlMassiva;
 int len;
-int kol=0;
-for (int k=0; k<4; k++)
+int kolKor=0;
+int kolDl=0;
+int k=0;
+while (k<dlMassiva)
 {
 len = res[k].Length;
-Console.WriteLine($"Длина {k} элемента массива: {len}");
+//Console.WriteLine($"Длина {k} элемента массива: {len}, ");
 if (len>3) 
-{
-    kol++;
-    for (int i = k + 1; i < res.Length; i++)
+    {
+        for (int i = k + 1; i < res.Length; i++)
         {
-            res[i - 1] = res[i];
+           res[i - 1] = res[i]; 
+            // Console.WriteLine($"00 {res[0]}");
+            // Console.WriteLine($"11 {res[1]}");
+            // Console.WriteLine($"22 {res[2]}");
+            // Console.WriteLine($"33 {res[3]}");
         }
-
-        // set the last element to zero
-        res[res.Length - 1] = "";
-
-        // print the modified array
-        Console.WriteLine("The modified array is:");
-        for (int i = 0; i < res.Length; i++)
-        {
-            
-            Console.Write(res[i] + " " + "i");
-        }
-
-
+       res[res.Length - 1] = "";
+            // Console.WriteLine($"0 {res[0]}");
+            // Console.WriteLine($"1 {res[1]}");
+            // Console.WriteLine($"2 {res[2]}");
+            // Console.WriteLine($"3 {res[3]}");
+    
+        k=k-1;
+        dlMassiva=dlMassiva-1;
+    
+    kolDl++; //Console.WriteLine($"kolDl {kolDl}");
     }
+    else kolKor++; //Console.WriteLine($"kolKor {kolKor}");
+
+k++;
 }
 
-Console.WriteLine($"Количество элементов массива длиной 3 и менее символов: {kol}");
-
-//string[] newArray = new string[kol];
-
-
-//string[] arr = { "1", "2", "3", "4", "5" };
-  //      int index = 2; // the index to start shifting from
-
-    //    for (int i = index + 1; i < arr.Length; i++)
-    //    {
-    //        arr[i - 1] = arr[i];
-    //    }
-
-        // set the last element to zero
-    //    arr[arr.Length - 1] = "";
-
-        // print the modified array
-    //    Console.WriteLine("The modified array is:");
-    //    for (int i = 0; i < arr.Length; i++)
-    //    {
-            
-    //        Console.Write(arr[i] + " " + "i");
-    //    }
+//Console.WriteLine($"Количество элементов массива длиной 3 и менее символов: {kol}");
 
 
 
 
 
-
-//string[] res = new string[4];
-
-//int[] originalArray = { 1, 2, 3, 4, 5 };
-            string[] newArray = new string[kol];
+            string[] newArray = new string[kolKor];
 
             for (int i = 0; i < newArray.Length; i++)
             {
                 newArray[i] = res[i];
             }
 
-Console.WriteLine($"Новый массив длиной {newArray.Length} из элементов менее 3 символов: ");
+Console.WriteLine($"Новый массив длиной {newArray.Length} из элементов длиной менее или равно 3 символов: ");
 
 for (int j=0; j<newArray.Length; j++)
 {
     Console.WriteLine($"{j} элемент массива: {newArray[j]}");
 }
-
-
-
-
-
-
-// List<string> numbers = new List<string> { "1", "2", "3", "4", "5" };
-//             int indexToRemove = 2; // remove the third element (index 2)
-
-//             numbers.RemoveAt(indexToRemove); // remove the element at index 2
-//            // for (int m=0; m<numbers.Length; m++)
-// Console.WriteLine($"нумб: {numbers[0]}");
-// Console.WriteLine($"нумб: {numbers[1]}");
-// Console.WriteLine($"нумб: {numbers[2]}");
-// Console.WriteLine($"нумб: {numbers[3]}");
-//Console.WriteLine($"нумб: {numbers[4]}");
+Console.WriteLine();
